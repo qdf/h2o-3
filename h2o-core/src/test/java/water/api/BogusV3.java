47fca756e2bebd8fff74c788d1658d2d53ebfaf6
mmalohlava <michal@h2o.ai>
2017-05-23 18:27:40 -0700

    Append missing schema into list of schemas.

---
+++ h2o-core/src/test/java/water/api/BogusV3.java
@@ -0,0 +1,8 @@
+package water.api;
+
+import hex.schemas.ModelBuilderSchema;
+import water.api.schemas3.ModelParametersSchemaV3;
+
+// Need this class, so a /3/Jobs can return the JSON'd version of it
+public class BogusV3 extends ModelBuilderSchema<Bogus,BogusV3,ModelParametersSchemaV3> {
+}
